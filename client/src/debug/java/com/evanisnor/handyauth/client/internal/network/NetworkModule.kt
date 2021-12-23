d68c32e4f76f7000cea405bf32e597e91a32be41
Evan Isnor <ewisnor@gmail.com>
2021-12-23 11:56:13 -0400

    Updating CI config

--- client/src/debug/java/com/evanisnor/handyauth/client/internal/network/NetworkModule.kt
+++ client/src/debug/java/com/evanisnor/handyauth/client/internal/network/NetworkModule.kt
@@ -1,10 +1,5 @@
 package com.evanisnor.handyauth.client.internal.network
 
-import com.evanisnor.handyauth.client.HandyAuthConfig
-import com.evanisnor.handyauth.client.internal.model.ExchangeResponseJsonAdapter
-import com.evanisnor.handyauth.client.internal.model.RefreshResponseJsonAdapter
-import com.evanisnor.handyauth.client.internal.secure.CodeGenerator
-import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
