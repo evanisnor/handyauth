492393003cc9167342ecccb522ff21f1442e8412
Evan Isnor <ewisnor@gmail.com>
2022-11-06 18:52:58 -0400

    Bumping target and compile sdk version to 33

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 31
+        targetSdk = 33
         consumerProguardFiles("consumer-rules.pro")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
