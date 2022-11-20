c0f3436395b4b3d9400b6f15a3b173f9f639d0b5
Evan Isnor <ewisnor@gmail.com>
2022-11-20 12:14:13 -0400

    Adding compat wrapper for queryIntentActivities

---
+++ client/src/main/java/com/evanisnor/handyauth/client/internal/ext/PackageManagerExt.kt
@@ -0,0 +1,17 @@
+package com.evanisnor.handyauth.client.internal.ext
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.os.Build
+
+fun PackageManager.queryIntentActivitiesCompat(
+  intent: Intent,
+  flags: Int,
+): MutableList<ResolveInfo> =
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(flags.toLong()))
+  } else {
+    @Suppress("DEPRECATION")
+    queryIntentActivities(intent, flags)
+  }
