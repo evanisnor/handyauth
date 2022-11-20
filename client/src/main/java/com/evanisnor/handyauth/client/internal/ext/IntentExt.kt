e7975adc9269defd6792ff98e71bc02941a205b1
Evan Isnor <ewisnor@gmail.com>
2022-11-20 11:51:16 -0400

    Adding compat wrapper for getParcelableExtra

---
+++ client/src/main/java/com/evanisnor/handyauth/client/internal/ext/IntentExt.kt
@@ -0,0 +1,13 @@
+package com.evanisnor.handyauth.client.internal.ext
+
+import android.content.Intent
+import android.os.Build
+import android.os.Parcelable
+import kotlin.reflect.KClass
+
+fun <T : Parcelable> Intent.getParcelableExtraCompat(name: String, clazz: KClass<T>) = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+  getParcelableExtra(name, clazz.java)
+} else {
+  @Suppress("DEPRECATION")
+  getParcelableExtra(name)
+}
